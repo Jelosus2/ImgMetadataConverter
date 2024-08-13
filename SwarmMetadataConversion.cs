4c7e5fd3d3aebe47799ac0c84b0efe33a5f668bd
Jelosus2 <navarrorodriguezjesus6@gmail.com>
2024-08-13 16:13:12 +0200

    Initial upload

---
+++ SwarmMetadataConversion.cs
@@ -0,0 +1,36 @@
+﻿using SwarmUI.Core;
+using SwarmUI.Utils;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Jelosus1.Extensions.SwarmMetadataConversion;
+
+public class SwarmMetadataConversion : Extension
+{
+
+    public override void OnInit()
+    {
+        Logs.Init("SwarmMetadataConverter is ready!");
+
+        string settingsFile = Path.Join(FilePath, "settings.json");
+
+        if (!Path.Exists(settingsFile))
+        {
+            Dictionary<string, string> defaultSettings = new()
+            {
+                ["cache"] = "active",
+                ["saveDirectory"] = "Output"
+            };
+
+            string jsonString = JsonConvert.SerializeObject(defaultSettings, Formatting.Indented);
+
+            File.WriteAllText(settingsFile, jsonString);
+        }
+
+        JObject settingsObj = JObject.Parse(File.ReadAllText(settingsFile));
+
+        Logs.Info(settingsObj["cache"].ToString());
+        Logs.Info(settingsObj["saveDirectory"].ToString());
+    }
+}
